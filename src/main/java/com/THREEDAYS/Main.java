a93e6be756e8f491d0a88ff824261bddd6456bfc
yintianan <1324783772@qq.com>
2019-12-10 15:41:42 +0800

    first

--- src/main/java/com/THREEDAYS/Main.java
+++ src/main/java/com/THREEDAYS/Main.java
@@ -3,7 +3,7 @@ package com.THREEDAYS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class Main {
+public class Main {//入口类
     public static  void main(String args[]){
         SpringApplication.run(Main.class, args);
     }
